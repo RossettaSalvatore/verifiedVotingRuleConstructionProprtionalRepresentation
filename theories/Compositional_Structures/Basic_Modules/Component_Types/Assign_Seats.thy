2b8b257095931962de2b268289939428abaad15a
RossettaSalvatore <ukolc@student.kit.edu>
2024-04-18 16:49:58 +0200

    moving code

---
+++ theories/Compositional_Structures/Basic_Modules/Component_Types/Assign_Seats.thy
@@ -0,0 +1,21 @@
+
+section \<open> Assign Seats \<close>
+
+theory Assign_Seats
+  imports Complex_Main
+Main
+"Social_Choice_Types/Preference_Relation"
+"Social_Choice_Types/Profile"
+"Social_Choice_Types/Result"
+"Electoral_Module"
+"Social_Choice_Types/Votes"
+"Termination_Condition"
+"HOL-Combinatorics.Multiset_Permutations"
+"Consensus_Class"
+"Distance"
+
+begin
+
+value "index [0::nat, 1, 3, 2] 3"
+
+end
--- theories/Compositional_Structures/Basic_Modules/Component_Types/Social_Choice_Types/Votes.thy
+++ theories/Compositional_Structures/Basic_Modules/Component_Types/Social_Choice_Types/Votes.thy
@@ -192,14 +192,12 @@ next
 qed
 *)
 
-text \<open> This function receives in input the function Votes and the list of parties. The 
-       output is the list of parties with the maximum number of votes.  \<close>
+text \<open> This function calculates the maximum number of votes. \<close>
 
 fun max_val_wrap:: "'a::linorder list \<Rightarrow> 'a::linorder" where 
 "max_val_wrap v = Max (set v)"
 
-(* i have to prove that for different votes almost the same but with m > v' > v
-   then m = m' *)
+
 lemma minus_max:
  assumes "ip = index parties party"
   "size v = size parties" and
@@ -207,50 +205,6 @@ lemma minus_max:
   "finite (set v)"
 shows "Max(set v) = Max ((set v) - {v ! ip})" 
   by (metis Diff_empty Diff_insert0 Max.remove assms(3) assms(4) max_def max_val_wrap.simps)
-
-(*
-lemma max_val_helper:
-  fixes
-  ip::"nat"
-  assumes "ip = index parties party"
-  assumes "x \<noteq> party \<Longrightarrow> v ! index parties x = v' ! index parties x"
-  assumes "v' ! ip > v ! ip" and "finite (set v)" and "finite (set v')" and
-  "size v = size parties" and "size v' = size parties" and
-  "max_val_wrap v \<noteq> v ! ip" and "max_val_wrap v' \<noteq> v' ! ip"
-shows "max_val_wrap v = max_val_wrap v'" 
-proof - 
-  assume "max_val_wrap v \<noteq> max_val_wrap v'" 
-  also have "max_val_wrap v = (Max(set v))" by simp
-  also have "max_val_wrap v' = (Max(set v'))" by simp
-  also obtain x where "v ! x = Max(set v)" 
-    using assms by (metis Max_in calculation empty_iff nth_equalityI nth_last_index nth_mem)
-  then obtain x' where "v' ! x' = Max(set v')" 
-   using assms by (metis Max_in calculation empty_iff nth_equalityI nth_last_index nth_mem)
-   then have "x \<noteq> ip" using assms
-  by (metis \<open>v ! x = Max (set v)\<close> max_val_wrap.simps)
-  then have "x' \<noteq> ip" using assms
-  by (metis \<open>v' ! x' = Max (set v')\<close> max_val_wrap.simps)
-  then have "v ! x \<noteq> v' ! x'" using assms \<open>v! x = Max(set v)\<close> 
-                                          \<open>v'! x' = Max(set v')\<close>
-                                          \<open> max_val_wrap v = Max(set v)\<close>
-                                          \<open> max_val_wrap v' = Max(set v')\<close>
-                                          \<open> max_val_wrap v \<noteq> max_val_wrap v'\<close> 
-    by presburger    
-  then have "x \<noteq> index parties party" using assms
-    using \<open>x \<noteq> ip\<close>
-  by metis
-  then have "parties ! x \<noteq> party" 
-    using assms max_val_helper_helper by presburger
-  then have "x' \<noteq> index parties party" using assms
-    using \<open>x' \<noteq> ip\<close> by blast
-  then have "parties ! x' \<noteq> party" 
-    using assms max_val_helper_helper by presburger
-  then have "parties ! x = parties ! x'"
-    using max_val_helper_helper by presburger
-  then have "v ! x = v' ! x'" using assms
-    by (metis max_val_helper_helper)
-  then show ?thesis sorry
-*)
 
 lemma max_val_wrap:
   fixes
@@ -278,27 +232,22 @@ lemma max_p_ne:
   shows "v ! index ps party \<noteq> m"
   using assms by simp
 
-(*
+
+text \<open> This lemma shows that max_p cannot be empty. \<close>
 lemma max_p_no_empty:
   fixes m::"rat" and v::"rat list" and parties::"'b list" and il::"nat list"
-  assumes "m = Max (set v)"
-  assumes "m > 0"
-  assumes "(set v) \<noteq> {}" and
-  "finite (set v)"
-  assumes "length v = length parties"
-  assumes "parties \<noteq> []"
-  assumes "\<forall>i j. i \<noteq> j \<Longrightarrow> parties ! i \<noteq> parties ! j"
+  assumes "m = Max (set v)" and
+  "(set v) \<noteq> {}" and
+  "length v = length parties" and
+  "\<And>i j. i \<noteq> j \<Longrightarrow> parties ! i \<noteq> parties ! j"
   shows "max_p m v parties w \<noteq> []"
 proof - 
-  have "m = Max (set v)" 
-    using assms by simp
-  then have "Max(set v) \<in> (set v)" 
-    using assms by simp
-  then have "\<exists>y. y \<in> set parties \<Longrightarrow> Max(set v) = v ! index parties y \<Longrightarrow> filter (\<lambda>x. v ! (index parties x) = Max(set v)) parties \<noteq> []" 
-    using assms by sledgehammer
+  have "Max(set v) \<in> (set v)" 
+    using assms(2) by auto
   then show ?thesis
-*)
-
+    using \<open>Max (set v) \<in> set v\<close> assms(1) assms(3) assms(4) empty_filter_conv in_set_conv_nth 
+          max_p.simps nth_index nth_mem by (smt (verit))
+qed
 
 lemma P_no:
   fixes m::"rat" and v::"rat list" and ps::"'b list"
@@ -357,9 +306,13 @@ lemma get_winners_loss:
   shows "party \<notin> set (get_winners v parties)"
   using assms by simp
 
+text \<open> This theorem proves that if a party has the max votes, then it is in the list of 
+        winners. \<close>
+
 theorem get_winners_in_win:
-  fixes fv::"'a::linorder list" and m::"'a"
-  assumes "fv ! (index ps px) = max_val_wrap fv" and 
+  fixes fv::"'a::linorder list"
+  assumes 
+    "fv ! (index ps px) = max_val_wrap fv" and 
     "px \<in> set ps"
   shows "px \<in> set (get_winners fv ps)"
 proof - have "get_winners fv ps = (let m = max_val_wrap fv in max_p m fv ps [])" 
@@ -412,8 +365,9 @@ lemma get_winners_only_winner:
   assumes "fv ! (index ps px) = max_val_wrap fv"
   and "\<forall>x \<noteq> index ps px. fv ! (index ps x) < max_val_wrap fv"  
   and "get_winners fv ps \<noteq> []"
-  shows "px = hd (get_winners fv ps)"
-  by (metis assms get_winners_not_in_win verit_comp_simplify1(1))
+shows "px = hd (get_winners fv ps)"
+  using get_winners_not_in_win verit_comp_simplify1(1) assms 
+  by metis
 
 (*
 lemma Max_ge [simp]:
@@ -427,37 +381,14 @@ lemma get_winners_weak_winner_implies_helper:
   assumes "x < size fv"
 shows "Max(set fv) \<ge> fv ! x" using assms by simp
 
-(*
-lemma Max_eqI:
-  assumes "finite A"
-  assumes "\<And>y. y \<in> A \<Longrightarrow> y \<le> x"
-    and "x \<in> A"
-  shows "Max A = x"
-*)
-lemma max_val_wrap_eqI:
+
+lemma max_val_wrap_eqI_helper:
   assumes "\<And>y. y \<in> (set fv) \<Longrightarrow> y \<le> fv ! x"
     and "fv ! x \<in> (set fv)"
   shows "Max(set fv) = fv ! x"
   using Max_eqI assms by blast
 
-lemma max_val_wrap_eqI_2:
-assumes 
-"\<And>x. fv ! index parties x \<in> (set fv) \<Longrightarrow> fv' ! index parties x \<le> fv ! index parties x"
-"fv' ! x \<in> (set fv')"
-shows "\<And>x. fv ! index parties x \<in> (set fv) \<Longrightarrow> fv' ! index parties x \<le> Max(set fv)"
-using Max_eqI
-  by (meson List.finite_set Max_ge assms dual_order.trans)
-
-lemma max_val_wrap_eqI_3:
-  assumes 
-"fv' ! index parties w \<in> (set fv')" and
-"fv ! index parties w = max_val_wrap fv" and
-"fv' ! index parties w > fv ! index parties w" 
-shows "max_val_wrap fv' > max_val_wrap fv"
-  using max_val_wrap.simps List.finite_set Max.coboundedI assms dual_order.strict_trans 
-        nless_le by metis
-
-lemma max_eqI_4:
+lemma max_eqI_helper:
   assumes 
 "x < length l" and 
 "length l = length l'" and
@@ -483,11 +414,11 @@ proof(rule ccontr)
           \<open>\<And>y. y \<noteq> x \<longrightarrow> y < length l \<longrightarrow> l' ! y < Max(set l')\<close> assms
           \<open>\<And>y. y \<noteq> x \<longrightarrow> y < length l \<longrightarrow> l' ! y \<le> Max (set l)\<close> 
           \<open>l' ! x \<noteq> Max (set l')\<close> in_set_conv_nth leD linorder_le_cases 
-          max_val_wrap_eqI order.strict_trans2
+          max_val_wrap_eqI_helper order.strict_trans2
   by (metis (no_types, opaque_lifting))
 qed
 
-lemma max_eqI_5:
+lemma max_eqI:
   fixes 
 fv::"rat list" and
 fv'::"rat list" and 
@@ -499,13 +430,14 @@ assumes
 "fv' ! index ps px > fv ! index ps px" and
 "\<And>y. y \<noteq> index ps px \<Longrightarrow> y < length fv \<Longrightarrow> fv' ! y \<le> fv ! y"
 shows "Max(set fv') = fv' ! index ps px"
-  by (metis assms max_eqI_4)
+  using assms max_eqI_helper by metis
+
+text \<open> This lemma proves that if a party wins, then if its votes increases, it wins again. \<close>
 
 lemma get_winners_weak_winner_implies:
   fixes 
 fv::"rat list" and
-fv'::"rat list" and 
-m'::"rat"
+fv'::"rat list"
 assumes
 "index ps px < length fv" and
 "length fv = length fv'" and
@@ -514,7 +446,7 @@ assumes
 "fv' ! index ps px > fv ! index ps px" and
 "\<And>y. y \<noteq> index ps px \<Longrightarrow> y < length fv \<Longrightarrow> fv' ! y \<le> fv ! y"
 shows "px \<in> set (get_winners fv' ps)"
-  by (metis assms get_winners_in_win max_eqI_4 max_val_wrap.simps)
+  using assms get_winners_in_win max_eqI_helper max_val_wrap.simps by metis
 
 lemma filter_size_is_one_helper:
   fixes
@@ -526,8 +458,8 @@ strict_le: "\<And>y. y \<noteq> x \<Longrightarrow> y < length fv \<Longrightarr
 shows "length (filter (\<lambda>x. fv ! x =  m) [0..<length fv]) = 1"
 proof -
   have le: \<open>[0..<length fv] = ([0..<x] @ [x] @ [Suc x ..< length fv])\<close>
-    using \<open>fv ! x = m\<close> append_Cons assms(1) leI le_add_diff_inverse less_imp_le_nat not_less_zero
-        self_append_conv2 upt_add_eq_append upt_rec
+    using \<open>fv ! x = m\<close> append_Cons assms(1) leI le_add_diff_inverse less_imp_le_nat 
+        not_less_zero self_append_conv2 upt_add_eq_append upt_rec
     by metis
   show \<open>length (filter (\<lambda>x. fv ! x =  m) [0..<length fv]) = 1\<close>
     using strict_le assms unfolding le by (force simp: filter_empty_conv)
@@ -544,36 +476,22 @@ strict_le: "\<And>y. y \<noteq> index ps x \<Longrightarrow> y < length fv \<Lon
 shows "length (filter (\<lambda>x. fv ! (index ps x) = m) ps) = 1"
   by (metis assms filter_size_is_one_helper)
 
-lemma filter_size_is_one_helper_my_case_2:
-  fixes
-fv::"rat list" and m::"rat" 
-assumes
-"index ps x < length fv" and
-"fv ! index ps x = m" and
-strict_le: "\<And>y. y \<noteq> index ps x \<Longrightarrow> y < length fv \<Longrightarrow> fv ! y < m" and 
-"length (filter (\<lambda>x. fv ! (index ps x) = m) ps) = length (filter (\<lambda>x. fv ! x =  m) [0..<length fv])"
-shows "length (max_p m fv ps []) = 1"
-proof - have "length (filter (\<lambda>x. fv ! x =  m) [0..<length fv]) = 1" 
-  using assms(1) assms(2) assms(3) filter_size_is_one_helper by metis
-  then show ?thesis 
-    using assms(4) max_p.simps by metis
-qed
-
 lemma filter_size_is_one_helper_my_case_3:
   fixes
-fv::"'a::linorder list" and m::"'a::linorder" 
+fv::"'a::linorder list"
 assumes
 "index ps x < length fv" and
-"m = Max(set fv)" and
 "fv ! index ps x = m" and
 strict_le: "\<And>y. y \<noteq> index ps x \<Longrightarrow> y < length fv \<Longrightarrow> fv ! y < m" and 
 "length (get_winners fv ps) = length (filter (\<lambda>x. fv ! x =  m) [0..<length fv])"
 shows "length (get_winners fv ps) = 1"
-  using assms(1) assms(3) assms(5) filter_size_is_one_helper strict_le by auto
+  using assms(1) assms(2) assms(4) filter_size_is_one_helper strict_le by auto
 
-lemma filter_size_is_one_helper_my_case_4:
+text \<open> This lemma states that if one the winner parties gets its votes increased, then it 
+       will be the only winner. \<close>
+lemma get_winners_size_one:
   fixes
-fv::"'a::linorder list" and m::"'a::linorder" 
+fv::"'a::linorder list"
 assumes
 "party \<in> set parties" and
 "index parties party < length fv" and
@@ -588,33 +506,26 @@ proof - have "length (get_winners fv parties) = 1"
   then have "party \<in> set (get_winners fv parties)" 
     using assms(1) assms(3) assms(4) get_winners_in_win max_val_wrap.elims by metis
   then show ?thesis 
-    using \<open>length (get_winners fv parties) = 1\<close> \<open>party \<in> set (get_winners fv parties)\<close> hd_conv_nth 
-          in_set_conv_nth length_greater_0_conv less_one by metis
+    using \<open>length (get_winners fv parties) = 1\<close> \<open>party \<in> set (get_winners fv parties)\<close> 
+          hd_conv_nth in_set_conv_nth length_greater_0_conv less_one by metis
 qed
 
-
-(*
-lemma get_winners_loss:
-  fixes m::"rat" and v::"rat list" and ps::"'b list"
-  assumes 
-    "party \<in> set parties" and
-    "(v ! (index parties party) \<noteq> Max(set v))"
-  shows "party \<notin> set (get_winners v parties)"
-  using assms by simp
-*)
+text \<open> This lemma states that if a party is in the head of winners, then it has the maximum
+       votes. \<close>
 
 lemma get_winners_rev:
-  assumes "party \<in> set ps" and 
-    "ps \<noteq> []" and
-    "size v = size ps" and
+  assumes
     "party = hd (get_winners v ps)" and "get_winners v ps \<noteq> []"
   shows "v ! index ps party = max_val_wrap v"
 proof(rule ccontr)
   assume "v ! index ps party \<noteq> max_val_wrap v"
-  then have "party \<notin> set (get_winners v ps)" using assms by simp
+  then have "party \<notin> set (get_winners v ps)" 
+    by simp
   then show False
-    using assms(4) assms(5) list.set_sel(1) by blast
+    using assms list.set_sel(1) by blast
 qed
+
+text \<open> This function assign the n-th seat to the winner. \<close>
 
 fun update_seat :: "'a::linorder \<Rightarrow> 'b list \<Rightarrow> ('a::linorder, 'b) Seats 
                     \<Rightarrow> ('a::linorder, 'b) Seats" where
